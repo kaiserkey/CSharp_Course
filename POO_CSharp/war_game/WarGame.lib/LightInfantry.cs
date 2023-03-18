a8bc886d2dec3bcc591066b76a9d5435f6467137
kaiserkey <kaiserkey2@gmail.com>
2023-03-18 20:55:44 -0300

    new changes

--- POO_CSharp/war_game/WarGame.lib/LightInfantry.cs
+++ POO_CSharp/war_game/WarGame.lib/LightInfantry.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace WarGame.lib;
 
-namespace WarGame.lib
+public class LightInfantry
 {
-    public class LightInfantry
-    {
-        
-    }
+    
 }
