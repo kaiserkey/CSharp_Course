83eec92cd1fda55a32ba2178efeb343bdc7eba2f
kaiserkey <kaiserkey2@gmail.com>
2023-03-18 20:49:44 -0300

    new changes

--- POO_CSharp/war_game/WarGame.lib/Soldier.cs
+++ POO_CSharp/war_game/WarGame.lib/Soldier.cs
@@ -3,7 +3,9 @@
 /* Esta clase es abstracta para que no se pueda instanciar y solo pueda ser heredada */
 public abstract class Soldier
 {
-    /* Le decimos a los metodos que sean protected para que  */
+    /* Le decimos a los metodos que sean protected para que  puedan ser accedidos por
+        la clase que heredan 
+    */
     protected abstract void Walk();
 
     protected abstract void Run();
