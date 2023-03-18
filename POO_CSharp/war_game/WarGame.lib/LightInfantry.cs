ed9b9cb14125b448f58d7b363c16760b4eb062e8
kaiserkey <kaiserkey2@gmail.com>
2023-03-18 20:57:34 -0300

    new changes

--- POO_CSharp/war_game/WarGame.lib/LightInfantry.cs
+++ POO_CSharp/war_game/WarGame.lib/LightInfantry.cs
@@ -3,7 +3,7 @@ namespace WarGame.lib;
 public class LightInfantry
 {
     /* Encapsulamos el comportamiento de nuestros metodos
-        publ
+        public, private, protected, internal
      */
     public void Shoot(){
 
