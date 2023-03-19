a03e18e3efefbdb544b86512230c444a06c8cc7e
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 00:09:54 -0300

    new changes

--- POO_CSharp/animals/Animal.cs
+++ POO_CSharp/animals/Animal.cs
@@ -8,12 +8,12 @@ namespace animals
     public class Animal
     {
         // Atributos
-        protected string Nombre { get; set; }
-        protected string Tipo { get; set; }
-        protected string Raza { get; set; }
+        protected string? Nombre { get; set; }
+        protected string? Tipo { get; set; }
+        protected string? Raza { get; set; }
 
         public Animal(){
-            
+
         }
     }
 }
