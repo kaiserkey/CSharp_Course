30f458fb40a03033cfb5667d6ccf14dacc160926
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 04:28:12 -0300

    new changes

--- NET/net_project/Controllers/PlaylistController.cs
+++ NET/net_project/Controllers/PlaylistController.cs
@@ -3,26 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 using net_project.Models;
 
 namespace net_project.Controllers;
+
+public class HomeController : Controller
 {
-    [Route("[controller]")]
-    public class PlaylistController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<PlaylistController> _logger;
+        _logger = logger;
+    }
 
-        public PlaylistController(ILogger<PlaylistController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Playlis()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
