45f33912dac4b4359a0ac9a689644850e8c5fc14
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 03:54:20 -0300

    new changes

---
+++ NET/net_project/Controllers/PlaylistController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace net_project.Controllers
+{
+    [Route("[controller]")]
+    public class PlaylistController : Controller
+    {
+        private readonly ILogger<PlaylistController> _logger;
+
+        public PlaylistController(ILogger<PlaylistController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
