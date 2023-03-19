cca0bc955e8f8d156463de85eb15af30b7966da6
kaiserkey <kaiserkey2@gmail.com>
2023-03-18 21:36:31 -0300

    new changes

--- POO_CSharp/automovil/Program.cs
+++ POO_CSharp/automovil/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace automovil;
 
 Auto auto1 = new Auto();
+
 
