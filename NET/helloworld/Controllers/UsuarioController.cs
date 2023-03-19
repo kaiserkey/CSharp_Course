24a1736c2a28b0499e0aa4a1bc326c6a703fa0ac
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 19:24:59 -0300

    new changes

--- NET/helloworld/Controllers/UsuarioController.cs
+++ NET/helloworld/Controllers/UsuarioController.cs
@@ -10,20 +10,9 @@ namespace helloworld.Controllers;
 
 public class UsuarioController : Controller
 {
-    private readonly ILogger<UsuarioController> _logger;
-
-    public UsuarioController(ILogger<UsuarioController> logger)
-    {
-        _logger = logger;
-    }
 
     public IActionResult Index(){
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
