130e513b972450a76e2fd1a1dd8f4feb0fc86152
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 19:17:54 -0300

    new changes

--- NET/helloworld/Controllers/UsuarioController.cs
+++ NET/helloworld/Controllers/UsuarioController.cs
@@ -1,15 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace helloworld.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsuarioController : ControllerBase
+    [Route("[controller]")]
+    public class UsuarioController : Controller
     {
-        
+        private readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
     }
 }
