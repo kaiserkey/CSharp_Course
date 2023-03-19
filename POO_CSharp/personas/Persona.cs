ec5d4ba4431a4c595a8f8f71be5ce6cae348dbcc
kaiserkey <kaiserkey2@gmail.com>
2023-03-18 22:37:54 -0300

    new changes

--- POO_CSharp/personas/Persona.cs
+++ POO_CSharp/personas/Persona.cs
@@ -12,6 +12,20 @@ namespace personas
         private int edad;
         private string dni;
         private string direccion;
-        
+        private string telefono;
+        private string email;
+        private string sexo;
+
+        public Persona()
+        {
+            nombre = "";
+            apellido = "";
+            edad = 0;
+            dni = "";
+            direccion = "";
+            telefono = "";
+            email = "";
+            sexo = "";
+        }
     }
 }
