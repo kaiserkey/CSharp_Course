c187bde0f36da0636869fdad96a63724447c1b97
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 17:24:30 -0300

    new changes

--- NET/helloworld/Controllers/HomeController.cs
+++ NET/helloworld/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult HelloWo()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
