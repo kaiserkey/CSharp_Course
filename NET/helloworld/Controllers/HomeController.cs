119e2cd0616246949a0c570bca61cd4f0d43d5bd
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 19:40:26 -0300

    new changes

--- NET/helloworld/Controllers/HomeController.cs
+++ NET/helloworld/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        
     }
 
     public IActionResult Index()
