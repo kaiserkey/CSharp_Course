e2de8f472807a7abf37af82273a7410ab656ee44
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 03:45:07 -0300

    new changes

--- NET/net_project/Program.cs
+++ NET/net_project/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<YoutubeContext>(options => 
+builder.Services.AddDbContext<YoutubeContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("MySQLContext")
     )
 );
