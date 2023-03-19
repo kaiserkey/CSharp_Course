37d921d50874023112d50aac45733a5aaf466eab
kaiserkey <kaiserkey2@gmail.com>
2023-03-18 21:00:32 -0300

    new changes

--- POO_CSharp/war_game/WarGame.lib/HeavyInfantry.cs
+++ POO_CSharp/war_game/WarGame.lib/HeavyInfantry.cs
@@ -3,6 +3,7 @@ namespace WarGame.lib;
 public class HeavyInfantry
 {
     public void Shoot(){
-        // aqui encapsulamos el comportamiento disparar, el cual sera un 
+        // aqui encapsulamos el comportamiento disparar, 
+        //el cual sera un poco distinto del de la clase LightInfantry
     }
 }
