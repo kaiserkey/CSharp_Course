7904e987d1e2f46f0fa7af67d696677d0c819683
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 03:52:02 -0300

    new changes

--- NET/net_project/Models/YoutubeContext.cs
+++ NET/net_project/Models/YoutubeContext.cs
@@ -19,10 +19,25 @@ public partial class YoutubeContext : DbContext
 
     public virtual DbSet<Video> Videos { get; set; }
 
-     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+/*      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseMySql("server=localhost;port=3306;database=Youtube;uid=root;password=1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
+ */
 
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+    if (!optionsBuilder.IsConfigured)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        optionsBuilder.UseMySql(
+            configuration.GetConnectionString("NombreDeTuCadenaDeConexion"),
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
+    }
+}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
