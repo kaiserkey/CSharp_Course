92dffc11f4c19a0d06e1038d70d9ff2eddf0923d
kaiserkey <kaiserkey2@gmail.com>
2023-03-18 23:15:48 -0300

    new changes

--- POO_CSharp/personas/Programador.cs
+++ POO_CSharp/personas/Programador.cs
@@ -7,6 +7,16 @@ namespace personas
 {
     public class Programador : Persona
     {
-        
+        private string Lenguaje { get; set; }
+
+        public Programador(string nombre, int edad, string genero, string lenguaje) : base(nombre, edad, genero)
+        {
+            Lenguaje = lenguaje;
+        }
+
+        public void Programar()
+        {
+            Console.WriteLine("{0} está programando en {1}.", Nombre, Lenguaje);
+        }
     }
 }
