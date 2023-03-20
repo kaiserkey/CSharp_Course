b73e14466885322b1f87632b097ca112d597150a
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 21:51:51 -0300

    new changes

--- NET/validations/Models/Usuario.cs
+++ NET/validations/Models/Usuario.cs
@@ -10,7 +10,7 @@ namespace validations.Models
     {
         [Required(ErrorMessage = "Escriba su nombre")]
         [MinLength(5, ErrorMessage = "El nombre debe tener al menos 5 caracteres")]
-        
+        [MaxLength(50, ErrorMessage = "El nombre debe tener máximo 20 caracteres")]
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string NickName { get; set; }
