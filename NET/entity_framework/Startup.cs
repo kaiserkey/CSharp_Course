fc24f063c8fb167a325741861ff5931971bda31a
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 13:04:29 -0300

    new changes

--- NET/entity_framework/Startup.cs
+++ NET/entity_framework/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using MySql.Data.EntityFrameworkCore.Extensions;
 
-namespace MyProject
+namespace entity_framework
 {
     public class Startup
     {
