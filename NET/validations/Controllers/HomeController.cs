b329b610362874fcb6215695c0a08b751339d2ae
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 10:53:01 -0300

    new changes

--- NET/validations/Controllers/HomeController.cs
+++ NET/validations/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public IActionResult SetUsuario(){
+            return View();
+    }
+
     public IActionResult Index()
     {
         return View();
