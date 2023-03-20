e87dc848fb8dbdadd463ac29c44e621a02cefeb3
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 20:20:53 -0300

    new changes

--- NET/entity_framework/Program.cs
+++ NET/entity_framework/Program.cs
@@ -1,5 +1,5 @@
 using Pomelo.EntityFrameworkCore.MySql;
-using entity_framework.Models;
+using entity_framework.Models.DBContext;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
