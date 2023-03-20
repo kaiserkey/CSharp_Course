ed6d4195452dde2cc5e184e2f02ed402f8c3ae5a
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 12:24:25 -0300

    new changes

--- NET/entity_framework/Startup.cs
+++ NET/entity_framework/Startup.cs
@@ -11,7 +11,7 @@ namespace entity_framework
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            var connection 
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=Inmobiliaria;Trusted_Connection=True;ConnectRetryCount=0";
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
