5212016a123fdcfd5949feb93060def52b971ec6
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 16:20:46 -0300

    new changes

--- NET/entity_framework/Models/DBContext.cs
+++ NET/entity_framework/Models/DBContext.cs
@@ -7,6 +7,11 @@ namespace entity_framework.Models
 {
     public class DBContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+  if (!optionsBuilder.IsConfigured)
+  { optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=;database=database");
+  }
+}
     }
 }
