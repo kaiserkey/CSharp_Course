c44e317cffbaff49c30ce437d93b171fe2b2ac80
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 12:46:12 -0300

    new changes

--- NET/entity_framework/Startup.cs
+++ NET/entity_framework/Startup.cs
@@ -1,28 +1,47 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using YourAppName.Models;
 
-namespace entity_framework
+namespace YourAppName
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuración de la cadena de conexión
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Configuración de la inyección de dependencias
+            services.AddScoped<IMyService, MyService>();
+
+            // Configuración de MVC y otros servicios
             services.AddControllersWithViews();
-            var connection = "Server=localhost;Database=Youtube;Uid=root;Pwd=1234;";
+            services.AddRazorPages();
+            services.AddHttpContextAccessor();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Configuración del entorno
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
@@ -36,6 +55,7 @@ namespace entity_framework
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
