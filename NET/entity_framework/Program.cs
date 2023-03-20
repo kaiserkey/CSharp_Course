100dd75cf9311fdc1ac88f7d852d642473635954
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 12:49:36 -0300

    new changes

--- NET/entity_framework/Program.cs
+++ NET/entity_framework/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+var startup = new Startup(builder.Configuration);
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
