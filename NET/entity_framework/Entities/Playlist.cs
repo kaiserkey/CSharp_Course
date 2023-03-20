b520e05a258c7559c7d2ea88e6288c24d3710eba
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 15:27:30 -0300

    new changes

---
+++ NET/entity_framework/Entities/Playlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace entity_framework.Entities;
+
+public partial class Playlist
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public int VideoId { get; set; }
+
+    public virtual Video Video { get; set; } = null!;
+}
---
+++ NET/entity_framework/Entities/Video.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace entity_framework.Entities;
+
+public partial class Video
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public virtual ICollection<Playlist> Playlists { get; } = new List<Playlist>();
+}
