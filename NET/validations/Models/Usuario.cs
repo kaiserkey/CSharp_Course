0cd505e4d0cb6af6c7c778032ccf180390e5d698
kaiserkey <kaiserkey2@gmail.com>
2023-03-19 21:50:43 -0300

    new changes

--- NET/validations/Models/Usuario.cs
+++ NET/validations/Models/Usuario.cs
@@ -1,14 +1,14 @@
-using System.ComponentModel.DataAnnotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System
 
 namespace validations.Models
 {
     public class Usuario
     {
-        [RequiredAttributered(ErrorMessage = "Escriba su nombre")]
+        [Required(ErrorMessage = "Escriba su nombre")]
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string NickName { get; set; }
