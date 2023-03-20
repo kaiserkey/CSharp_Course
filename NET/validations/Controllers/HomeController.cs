6b483023a33a50988d0998106775c285c240231e
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 10:57:46 -0300

    new changes

--- NET/validations/Controllers/HomeController.cs
+++ NET/validations/Controllers/HomeController.cs
@@ -16,6 +16,8 @@ public class HomeController : Controller
     [BindProperty]
     public Usuario? _Usuario { get; set; }
     public IActionResult SetUsuario(){
+        _Usuario = new Usuario();
+        
         return Json(_Usuario);
     }
 
