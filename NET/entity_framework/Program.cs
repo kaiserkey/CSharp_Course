2bae9c598c3574d29a93f4016c9d265d6780d6f6
kaiserkey <kaiserkey2@gmail.com>
2023-03-20 21:23:38 -0300

    new changes

--- NET/entity_framework/Program.cs
+++ NET/entity_framework/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MySql.EntityFrameworkCore.Extensions;
 using entity_framework.Models;
 using MySql.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
