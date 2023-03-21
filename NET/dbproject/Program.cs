27bcedab6ecfded1e7487fe1adec4fa191720cb6
kaiserkey <kaiserkey2@gmail.com>
2023-03-21 09:02:09 -0300

    new changes

--- NET/dbproject/Program.cs
+++ NET/dbproject/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 using dbproject.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
