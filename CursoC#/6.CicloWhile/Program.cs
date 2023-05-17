7cdc7c9aafaa8241e98ab04d178b185202a1b721
kaiserkey <kaiserkey2@gmail.com>
2023-05-17 20:36:08 -0300

    new changes

---
+++ CursoC#/6.CicloWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _6.CicloWhile
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ciclo while
+
+            int i = 0;
+            while (i < 10)
+            {
+                System.Console.WriteLine("Contador del While: "+i);
+                i++;
+            }
+
+
+            do
+            {
+                System.Console.WriteLine("Contador del Do-While: "+i);
+                i++;
+            } while (i < 10);
+
+
+        }
+    }
+}
