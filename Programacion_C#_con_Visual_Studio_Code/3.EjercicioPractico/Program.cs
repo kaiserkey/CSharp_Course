60676426a9bc2733e525874dd7e4dadba825a7cb
kaiserkey <kaiserkey2@gmail.com>
2023-05-23 16:44:50 -0300

    new changes

--- Programacion_C#_con_Visual_Studio_Code/3.EjercicioPractico/Program.cs
+++ Programacion_C#_con_Visual_Studio_Code/3.EjercicioPractico/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace _3.EjercicioPractico
+﻿using Internal;
+using System;
+namespace _3.EjercicioPractico
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            string nombre, apellido, hobby;
+            int edad;
+
+            Console.Write("Introduce tu nombre/s: ");
+            nombre = Console.ReadLine();
+            Console.Write("Introduce tu apellido/s: ");
+            apellido = Console.ReadLine();
+            Console.Write("Introduce tu edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
             
         }
     }
