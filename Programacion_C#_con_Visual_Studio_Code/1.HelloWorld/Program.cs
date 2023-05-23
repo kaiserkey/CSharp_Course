56f86c50b3720eb84fc534e5f08925117fbb38d6
kaiserkey <kaiserkey2@gmail.com>
2023-05-23 10:16:08 -0300

    new changes

--- Programacion_C#_con_Visual_Studio_Code/1.HelloWorld/Program.cs
+++ Programacion_C#_con_Visual_Studio_Code/1.HelloWorld/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace _1.HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
